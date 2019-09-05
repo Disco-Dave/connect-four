e61ab2d2c7ec8118fd845d483c9c1f7a228ee63f
David Burkett <db.discodave@gmail.com>
2019-09-05 18:03:47 -0400

    Wrote a function to check if board is full

--- src/Data/State/Game.hs
+++ src/Data/State/Game.hs
@@ -8,6 +8,8 @@ module Data.State.Game
 where
 
 import           Prelude
+import           Data.Monoid                    ( All(..) )
+import           Data.Maybe                     ( isJust )
 import qualified Data.State.Board              as Board
 import           Data.State.Board               ( Board )
 
@@ -33,20 +35,22 @@ playTurn column gameState@(OnGoing player board) =
   let chip = playerToChip player
   in  case Board.dropInto chip column board of
         Board.UpdateFailed              -> gameState
-        Board.UpdateSuccessful newBoard -> 
-          case findAWin newBoard of
-            Just (winningPlayer, winningSlots) ->
-              Win winningPlayer winningSlots newBoard
-            _ -> if isBoardFull newBoard
-              then Tie newBoard
-              else OnGoing (otherPlayer player) newBoard
+        Board.UpdateSuccessful newBoard -> case findAWin newBoard of
+          Just (winningPlayer, winningSlots) ->
+            Win winningPlayer winningSlots newBoard
+          _ -> if isBoardFull newBoard
+            then Tie newBoard
+            else OnGoing (otherPlayer player) newBoard
 playTurn _ gameState = gameState
 
 isBoardFull :: Board -> Bool
-isBoardFull = _
+isBoardFull board =
+  let columns = flip Board.column board <$> [minBound .. maxBound]
+  in  getAll . mconcat $ All . all isJust <$> columns
 
 findAWin :: Board -> Maybe (Player, WinningSlots)
 findAWin = _
+
 
 playerToChip :: Player -> Board.Chip
 playerToChip PlayerOne = Board.RedChip
