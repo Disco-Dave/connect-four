19e089e5e09ee6310d3037dde6c36175d22cf841
David Burkett <db.discodave@gmail.com>
2019-09-09 19:43:17 -0400

    Deleted all code to start fresh

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,6 @@
 module Main where
 
 import           Prelude
-import           Graphics.Gloss
-import           Game
-import qualified Data.State.Game               as GameState
 
 main :: IO ()
-main = play (InWindow "Connect Four" (700, 600) (10, 10))
-            yellow
-            0
-            GameState.init
-            view
-            glossUpdate
-            (const id)
-
-
-
-picture :: Picture
-picture = Translate 100 200 (Circle 80) <> Translate 10 10 (Circle 10)
-
-
+main = putTextLn "..."
