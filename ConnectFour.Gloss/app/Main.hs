ac649f7627c7ff3663e7e1db780ac8d5d610b7c8
David Burkett <db.discodave@gmail.com>
2019-09-18 20:10:00 -0400

    Keep track of the size of the window for resizing shapes dynamically

--- ConnectFour.Gloss/app/Main.hs
+++ ConnectFour.Gloss/app/Main.hs
@@ -1,18 +1,33 @@
 module Main where
 
-import           Relude
+import           Relude                  hiding ( init )
 
 import           Graphics.Gloss
+import           Graphics.Gloss.Interface.IO.Interact
 
 main :: IO ()
-main = 
-  play
-    (InWindow "Connect Four" (700, 600) (10, 10))
-    black
-    0
-    ()
-    mempty
-    (const id)
-    (const id)
+main = play (InWindow "Connect Four" (700, 600) (10, 10))
+            black
+            0
+            init
+            mempty
+            handleEvent
+            (const id)
 
-    
+init :: World
+init = World { viewPort = ViewPort 600 700 }
+
+newtype World = World
+  { viewPort :: ViewPort
+  } deriving Show
+
+data ViewPort = ViewPort
+  { viewPortLength :: Int
+  , viewPortWidth :: Int
+  } deriving Show
+
+handleEvent :: Event -> World -> World
+handleEvent (EventResize (w, l)) oldWorld =
+  let newWorld = oldWorld { viewPort = ViewPort l w }
+  in  traceShow newWorld newWorld
+handleEvent _ v = v
