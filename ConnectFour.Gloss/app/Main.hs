631bef44098b70de1ee4453e02453f3d73601e27
David Burkett <db.discodave@gmail.com>
2019-09-21 23:16:51 -0400

    Figured out the resizing of the square

--- ConnectFour.Gloss/app/Main.hs
+++ ConnectFour.Gloss/app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import           Relude                  hiding ( init )
@@ -10,7 +12,7 @@ main = play (InWindow "Connect Four" (700, 600) (10, 10))
             black
             0
             init
-            mempty
+            drawPicture
             handleEvent
             (const id)
 
@@ -31,3 +33,20 @@ handleEvent (EventResize (w, l)) oldWorld =
   let newWorld = oldWorld { viewPort = ViewPort l w }
   in  traceShow newWorld newWorld
 handleEvent _ v = v
+
+drawPicture :: World -> Picture
+drawPicture World {..} =
+  let width'  = viewPortWidth viewPort
+      length' = viewPortLength viewPort
+      (boardWidth, boardLength) =
+          if (toRational width' / toRational length') <= (7.0 / 6.0)
+            then
+              let boardWidth'  = fromIntegral width'
+                  boardLength' = fromRational $ toRational width' * (6 / 7)
+              in  (boardWidth', boardLength')
+            else
+              let boardWidth'  = fromRational $ toRational length' * (7 / 6)
+                  boardLength' = fromIntegral length'
+              in  (boardWidth', boardLength')
+  in  color yellow $ rectangleSolid boardWidth boardLength
+
