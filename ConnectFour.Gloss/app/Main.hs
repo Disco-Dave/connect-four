15ab66fcc10ce18e6ccf239e011ea6dfab71dc87
David Burkett <db.discodave@gmail.com>
2019-09-22 19:04:01 -0400

    Gave up the idea of scaling the board for now on input change

--- ConnectFour.Gloss/app/Main.hs
+++ ConnectFour.Gloss/app/Main.hs
@@ -7,8 +7,10 @@ import           Relude                  hiding ( init )
 import           Graphics.Gloss
 import           Graphics.Gloss.Interface.IO.Interact
 
+import           Data.Ratio
+
 main :: IO ()
-main = play (InWindow "Connect Four" (700, 600) (10, 10))
+main = play (InWindow "Connect Four" (700, 660) (10, 10))
             black
             0
             init
@@ -17,36 +19,22 @@ main = play (InWindow "Connect Four" (700, 600) (10, 10))
             (const id)
 
 init :: World
-init = World { viewPort = ViewPort 600 700 }
+init = World
 
-newtype World = World
-  { viewPort :: ViewPort
-  } deriving Show
-
-data ViewPort = ViewPort
-  { viewPortLength :: Int
-  , viewPortWidth :: Int
-  } deriving Show
+data World = World
 
 handleEvent :: Event -> World -> World
-handleEvent (EventResize (w, l)) oldWorld =
-  let newWorld = oldWorld { viewPort = ViewPort l w }
-  in  traceShow newWorld newWorld
 handleEvent _ v = v
 
-drawPicture :: World -> Picture
-drawPicture World {..} =
-  let width'  = viewPortWidth viewPort
-      length' = viewPortLength viewPort
-      (boardWidth, boardLength) =
-          if (toRational width' / toRational length') <= (7.0 / 6.0)
-            then
-              let boardWidth'  = fromIntegral width'
-                  boardLength' = fromRational $ toRational width' * (6 / 7)
-              in  (boardWidth', boardLength')
-            else
-              let boardWidth'  = fromRational $ toRational length' * (7 / 6)
-                  boardLength' = fromIntegral length'
-              in  (boardWidth', boardLength')
-  in  color yellow $ rectangleSolid boardWidth boardLength
 
+{- VIEW CODE -}
+
+drawPicture :: World -> Picture
+drawPicture world = Pictures [drawBoard world, drawCommandBar world]
+
+
+drawBoard :: World -> Picture
+drawBoard _ = translate 0 (-30) $ color yellow $ rectangleSolid 700 600
+
+drawCommandBar :: World -> Picture
+drawCommandBar _ = translate 0 300 $ color red $ rectangleSolid 700 60
